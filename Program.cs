276497878f4bc9c9fd76c4e8967b9d1d6ba3b9e0
seancrowe <retrounlocked@gmail.com>
2020-10-11 22:30:17 -0500

    Add project files

---
+++ Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Schema;
+using CsvHelper;
+
+namespace NamingPackages
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory);
+            var allChecksPassed = true;
+
+            if (!Directory.Exists($"{currentDirectory}/unpack"))
+            {
+                Directory.CreateDirectory($"{currentDirectory}/unpack");
+                Console.WriteLine("unpack directory does not exist");
+                allChecksPassed = false;
+            }
+
+            if (!Directory.Exists($"{currentDirectory}/output"))
+            {
+                Directory.CreateDirectory($"{currentDirectory}/output");
+            }
+
+            if (!Directory.Exists($"{currentDirectory}/config"))
+            {
+                Directory.CreateDirectory($"{currentDirectory}/config");
+                Console.WriteLine("pattern.xml does not exist");
+                allChecksPassed = false;
+            }           
+            
+            if (!File.Exists($"{currentDirectory}/config/pattern.xml"))
+            {
+                File.WriteAllText($"{currentDirectory}/config/pattern.xml", "<patterns><pattern name=\"Name Of Zip\">example_%var_COMPANY%_%var_MONTH%_%var_MARKET%_%var_LAYOUT%</pattern></patterns>");
+                Console.WriteLine("no pattern.xml - I created one for you");
+                allChecksPassed = false;
+            }
+
+            if (Directory.Exists($"{currentDirectory}/temp"))
+            {
+                CleanUpTemp(currentDirectory);
+            }
+            else
+            {
+                Directory.CreateDirectory($"{currentDirectory}/temp");
+            }
+
+            if (allChecksPassed)
+            {
+
+                var patternXmlDocument = new XmlDocument();
+                try
+                {
+                    patternXmlDocument.Load($"{currentDirectory}/config/pattern.xml");
+                }
+                catch
+                {
+                    Console.WriteLine("cannot parse pattern.xml - something wrong with xml");
+                    Environment.Exit(0);
+                }
+
+
+                var zipDirectoryInfo = new DirectoryInfo($"{currentDirectory}/unpack/");
+
+                var zipFiles = zipDirectoryInfo.GetFiles().Where(fileInfo => fileInfo.Name.Contains(".zip"));
+
+                foreach (var zipFile in zipFiles)
+                {
+                    ConvertZipImages(zipFile, currentDirectory, patternXmlDocument);
+                }
+
+                CleanUpTemp(currentDirectory);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+
+        }
+
+        static void CleanUpTemp(string currentDirectory)
+        {
+            if (!Directory.Exists($"{currentDirectory}/temp"))
+            {
+                return;
+            }
+            
+            var files = Directory.GetFiles($"{currentDirectory}/temp");
+
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+        }
+
+        static (string[], List<string>) GetPatternDetails(string patternText)
+        {
+            var patternSplitArray = patternText.Split("%");
+            var variableList = new List<string>();
+
+            for (int i = 0; i < patternSplitArray.Length; i++)
+            {
+                var subtext = patternSplitArray[i];
+                
+                if (subtext.Contains("var_"))
+                {
+                    patternSplitArray[i] = subtext.Substring(4);
+                    variableList.Add(subtext.Substring(4));
+                }
+            }
+
+            return (patternSplitArray, variableList);
+        }
+        
+        static void ConvertZipImages(FileInfo zipFile, string currentDirectory, XmlDocument patternXmlDocument)
+        {
+
+            string csvPath = null;
+            string fileName = zipFile.Name.Replace(".zip", "");
+            
+            Console.WriteLine($"processing {fileName}.zip");
+
+            string patternString = null;
+            foreach (XmlNode node in patternXmlDocument.SelectNodes("//patterns/pattern"))
+            {
+                if (node.Attributes["name"] != null)
+                {
+                    if (node.Attributes["name"].Value == fileName)
+                    {
+                        patternString = node.InnerText;
+                        break;
+                    }
+                }
+            };
+
+            if (patternString == null)
+            {
+                Console.WriteLine($"cannot find pattern for {fileName}.zip --skipping");
+                return;
+            }
+
+            var patternTuple = GetPatternDetails(patternString);
+
+            var patternSplitArray = patternTuple.Item1;
+            var variables = patternTuple.Item2;
+            
+            using (FileStream zipToOpen = new FileStream(zipFile.FullName, FileMode.Open))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        if (entry.FullName.Contains(".csv"))
+                        {
+                            csvPath = $"{currentDirectory}/temp/{entry.FullName}";
+                        }
+
+                        entry.ExtractToFile($"{currentDirectory}/temp/{entry.FullName}");
+                    }
+                }
+            }
+
+            if (csvPath == null)
+            {
+                System.Environment.Exit(0);
+            }
+
+            var tempDirectoryInfo = new DirectoryInfo($"{currentDirectory}/temp/");
+            var imageFiles = tempDirectoryInfo.GetFiles().Where(imageFile => !imageFile.Name.Contains(".csv")).ToList();
+
+            if (Directory.Exists($"{currentDirectory}/output/{fileName}"))
+            {
+                var files = Directory.GetFiles($"{currentDirectory}/output/{fileName}");
+
+                foreach (var file in files)
+                {
+                    File.Delete(file);
+                }
+            }
+            
+            var outputDirectoryInfo = Directory.CreateDirectory($"{currentDirectory}/output/{fileName}");
+            
+            using (var reader = new StreamReader(csvPath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var recordsList = csv.GetRecords<dynamic>().ToList();
+
+                for (int i = 0; i < recordsList.Count && i < imageFiles.Count; i++)
+                {
+                    var record = recordsList[i];
+                    var imageFile = imageFiles[i];
+
+                    string imageName = "";
+                    
+                    foreach (var strPattern in patternSplitArray)
+                    {
+                        if (variables.Contains(strPattern))
+                        {
+                            
+                            foreach (var data in record)
+                            {
+                                if (data.Key == strPattern)
+                                {
+                                    imageName += data.Value;
+                                }
+                            }
+                            
+                            continue;
+                        }
+
+                        imageName += strPattern;
+                    }
+
+                    var extension = imageFile.Extension;
+                    var fullName = outputDirectoryInfo.FullName + $"/{imageName}{extension}";
+
+                    if (!File.Exists(fullName)) ;
+                    
+                    File.Copy(imageFile.FullName, fullName);
+                }
+                
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,77 @@
+﻿### Warning
+This script was created in under 3 hours and thus consider it not production ready missing many optimizations and error handling.
+
+### Reason
+This script was created to rename datasource output from CHILI publisher using the naming pattern found in CHILI publisher products.
+
+This was developed as a workaround but could potentially be used with older versions of CHILI publisher that do not support pattern naming of datasource output.
+
+Finally, this could be forked and reworked to provide advance pattern naming that cannot be done in CHILI publisher.
+
+### Limitations
+Currently this script only supports $var_XXX% of the naming pattern introduced in CHILI publisher 5.7.
+
+### Build
+You can build this application by running
+```shell
+dotnet publish -r {RID} -p:PublishSingleFile=true --self-contained true -o {location}
+```
+
+After build, you should create an unpack and config folder then add the pattern.xml. Running the application will also create these folders/files for you.
+
+------------
+
+
+## Use
+### Getting Output Package
+This script will rename datasource output from CHILI publisher. Therefore you will need to provide a zip package of this output.
+
+The following are requirements of your PDF Export Settings to make a compatible output. For Image Conversion Profiles, if you are doing an image ouput instead of a PDF output, the settings do not matter. However, you must have the following in your PDF Export Settings whether you are doing PDF output or image output.
+
+The below settings will be found in the DataSource tab of your PDF Export Settings.
+
+- PDF Naming Pattern - it must be empty (this may not exist in older versions)
+- Max Rows - set to -1
+- Rows / PDF - set to 1
+- Create seperate background PDF - unchecked
+- Include backgroubd layers in variable PDF - unchecked
+- Include creation log - unchecked
+- Export datasource - **checked**
+
+If any of the above settings do not match, the script will not be able to process the output zip.
+
+### Renaming Output
+Once you have the output zip, place the zip file in the unpack folder of the application directory. Then in the config folder you will find a pattern.xml file.
+
+#### Pattern.xml Configuration
+You must add a `<pattern></pattern>` element with a name attribute that is equal to the output zip.
+
+The inner text value of the pattern tag should match the PDF Name Pattern that would be used in CHILI publicher 5.7 or above.
+
+Currently only %var_XXX% is supported. 
+
+For example, if your output zip is called Example.zip and the document outputted has two variables you want in the file name:
+- First Name
+- Last Name
+
+You would then make configer the pattern.xml as the following:
+```xml
+<patterns>
+    <pattern name="Example">example_%var_First NameY%_%Last Name%</pattern>
+</patterns>
+```
+
+If you want to process more than one output, it is very simple. Just place the zip packages in the unpack folder and add more `<pattern></pattern>` elements to the pattern.xml.
+
+For example, if you have to outputs to process: Example.zip and Diff Example.zip your pattern.xml would look like:
+```xml
+<patterns>
+    <pattern name="Example">example_%var_First NameY%_%Last Name%</pattern>
+	<pattern name="Diff Example">example_%var_Another Variable%_%CASE SENSITIVE%</pattern>
+</patterns>
+```
+
+**Note!!!** Variables are case sensitive to the name in the datasource column name. Please make sure the name used in your pattern matching is the exact match in the datasource column.
+&nbsp;
+&nbsp;
+Once complete, your output will be found in the output folder.
